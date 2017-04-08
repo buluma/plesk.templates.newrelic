7c182bd6b856a371665954c1ff97f5739af0467e
Michael Buluma <buluma@users.noreply.github.com>
2017-04-08 09:28:32 +0300

    Update Custom php.php
    Syntax and clean up.

--- psa/admin/conf/templates/custom/service/php.php
+++ psa/admin/conf/templates/custom/service/php.php
@@ -11,6 +11,7 @@ if ($OPT['enabled']) {
     echo "php_admin_flag engine off\n";
 }
 ?>
+
 <?php
 if (isset($VAR->domain))
 {
@@ -23,15 +24,14 @@ if (isset($VAR->domain))
 <?php
 if (array_key_exists('enabled', $OPT) && $OPT['enabled']) {
     echo "php_admin_flag engine on\n";
-
     if (isset($OPT['settings'])) {
         echo $OPT['settings'];
     }
-
 } else {
     echo "php_admin_flag engine off\n";
 }
 ?>
+
 <?php
 if (isset($VAR->domain))
 {
